a3de913b8d93e784d64921161ae67c5b502942b1
Matthias Reisacher <matt@shapediver.com>
2025-06-12 13:51:50 +0200

    Publish v1.9.0

--- src/shapediver/geometry_api_v2/sd_client.py
+++ src/shapediver/geometry_api_v2/sd_client.py
@@ -16,7 +16,7 @@ class SdClient(ApiClient):
         retries.
     """
 
-    _sdk_version = "1.8.0"  # WARNING: This value is updated automatically!
+    _sdk_version = "1.9.0"  # WARNING: This value is updated automatically!
 
     def __init__(
         self,
