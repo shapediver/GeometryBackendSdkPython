1361f597c747d99043e42f315f096f1bab41f1d2
Matthias Reisacher <matt@shapediver.com>
2026-06-29 18:07:08 +0200

    Generate spec version 2.4.0

--- tests/test_analytics_api.py
+++ tests/test_analytics_api.py
@@ -5,35 +5,9 @@ from shapediver.geometry_api_v2 import (
     ReqCreditMetric,
     ReqCreditMetrics,
     ReqModelCreditMetricId,
-    ReqModelStatistic,
-    ReqModelStatistics,
     SdClient,
     SessionApi,
 )
-
-
-def test_model_session_statistics(utils, host, jwt_model, model_id):
-    model_client = SdClient(Configuration(host, access_token=jwt_model))
-
-    # Initialize a new session.
-    ticket = utils.create_ticket()
-    session_id = SessionApi(model_client).create_session_by_ticket(ticket).session_id
-
-    # Fetch model statistics within a specific time range.
-    req_stats = ReqModelStatistics(
-        parameters=[
-            ReqModelStatistic(
-                modelid=[model_id],
-                timestamp_from="2024",
-                timestamp_to="2025",
-            )
-        ]
-    )
-    res_stats = AnalyticsApi(model_client).get_model_statistics(req_stats)
-    assert len(res_stats.analytics.models) > 0
-
-    # Close the session.
-    SessionApi(model_client).close_session(session_id)
 
 
 def test_credit_metrics(utils, host, jwt_model, model_id):
